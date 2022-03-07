655d403ff524659573219159fa9011a685bd716d
DAsm <zh.daur@gmail.com>
2022-03-07 12:44:14 +0600

    useragent and skeleton of kaspi

--- kaspi_kz_scrappy/settings.py
+++ kaspi_kz_scrappy/settings.py
@@ -14,10 +14,10 @@ NEWSPIDER_MODULE = 'kaspi_kz_scrappy.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'kaspi_kz_scrappy (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
--- kaspi_kz_scrappy/spiders/kaspi_spider.py
+++ kaspi_kz_scrappy/spiders/kaspi_spider.py
@@ -5,15 +5,49 @@ class KaspiSpider(scrapy.Spider):
 
     def start_requests(self):
         urls = [
-            'https://quotes.toscrape.com/page/1/',
-            'https://quotes.toscrape.com/page/2/',
-        ]
+            'https://kaspi.kz/shop/c/categories/',        ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = f'quotes-{page}.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log(f'Saved file {filename}')
+        # get all links from response
+        links = response.css('a::attr(href)').extract()
+
+        self.log('123412341234123412341234123412341234')
+        for link in links:
+            if link.startswith('/shop/c/categories/'):
+                self.log(f'link {link}')
+                yield response.follow(link, callback=self.parse_category)
+
+    def parse_category(self, response):
+        # get all links from response
+        links = response.css('a::attr(href)').extract()
+        for link in links:
+            if link.startswith('/shop/p/'):
+                yield response.follow(link, callback=self.parse_product)  
+
+    def parse_product(self, response):
+        # print response
+ 
+        title = response.css('item__heading::text').extract_first()
+        price = response.css('div.item__price-once::text').get()
+        description = response.css('div.product-description::text').extract_first()
+        image_url = response.css('img.product-image::attr(src)').extract_first()
+        print(f'{title} {price} {description} {image_url}')
+        self.log(f'title {title}')
+        self.log(f'price {price}')
+        self.log(f'description {description}')
+        self.log(f'image_url {image_url}')
+
+        #yield {
+        #    'title': response.css('h1::text').extract_first(),
+        #    'price': response.css('span.price::text').extract_first(),
+        #    'description': response.css('div.description::text').extract_first(),
+        #    'image_urls': response.css('img::attr(src)').extract(),
+        #}
+
+        #page = response.url.split("/")[-2]
+        #filename = f'quotes-{page}.html'
+        #with open(filename, 'wb') as f:
+        #   f.write(response.body)
+        #self.log(f'Saved file {filename}')
